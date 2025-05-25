9d3ef62f6ea71be13d131598de2e9ca0229d68df
Craig <craigpinto17@gmail.com>
2025-05-25 13:50:13 -0400

    Initial Commit Renaming to HomebridgeSwidget Plugin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import type { API } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform.js';
 import { PLATFORM_NAME } from './settings.js';
+import { SwidgetHomebridgePlatform } from './platform.js';
 
 /**
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, SwidgetHomebridgePlatform);
 };
